1fe226218fa853926a91ef8d7de89322d63dd4fa
Salil Biswas <biswass10@my.sunyulster.edu>
2018-08-09 21:03:53 +0000

    Depot Scaffold with pg and rails_12factor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   root 'products#index'
+  # make the home page as the products page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
